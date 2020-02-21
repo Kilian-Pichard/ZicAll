0a8a6be0fad9fedb8c7f23d9f8fb1c9b3cf4d259
Christopher <mirambeau.lcd@gmail.com>
2020-02-21 11:15:45 +0100

    Début de programmation de la fonctionnalité 'Apprentissage du piano'

--- app/src/main/java/iutbayonne/projet/zicall/MainActivity.java
+++ app/src/main/java/iutbayonne/projet/zicall/MainActivity.java
@@ -2,12 +2,124 @@ package iutbayonne.projet.zicall;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
+    private static final String LOG_TAG = "PianoTest";
+    private Button start;
+    private ImageView touche;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.start = findViewById(R.id.start);
+
+        this.start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                new JouerMelodie();
+            }
+        });
+
     }
+
+    private void activerTouche()
+    {
+        this.touche.setImageResource(R.drawable.touche_piano_allumee);
+        Log.e(LOG_TAG, "Activation d'une touche");
+    }
+
+    private void desactiverTouche(boolean estNoire)
+    {
+        this.touche.setImageResource(
+                estNoire ? R.drawable.touche_noire : R.drawable.touche_piano );
+        Log.e(LOG_TAG, "Desactivation d'une touche");
+    }
+
+    public class JouerMelodie extends Thread
+    {
+
+        public JouerMelodie()
+        {
+            start();
+        }
+
+        public void run()
+        {
+
+            Note[] notesBellaCiao = {
+                    new Note(Clavier.MI_GAMME_2, (float) 0.5),
+                    new Note(Clavier.LA_GAMME_2, (float) 0.5),
+                    new Note(Clavier.SI_GAMME_2, (float) 0.5),
+                    new Note(Clavier.DO_GAMME_2, (float) 0.5),
+                    new Note(Clavier.LA_GAMME_2, 2),
+                    new Note(Clavier.MI_GAMME_2, (float) 0.5),
+                    new Note(Clavier.LA_GAMME_2, (float) 0.5),
+                    new Note(Clavier.SI_GAMME_2, (float) 0.5),
+                    new Note(Clavier.DO_GAMME_2, (float) 0.5),
+                    new Note(Clavier.LA_GAMME_2, 2),
+                    new Note(Clavier.MI_GAMME_2, (float) 0.5),
+                    new Note(Clavier.LA_GAMME_2, (float) 0.5),
+                    new Note(Clavier.SI_GAMME_2, (float) 0.5),
+                    new Note(Clavier.DO_GAMME_2, 1),
+                    new Note(Clavier.SI_GAMME_2, (float) 0.5),
+                    new Note(Clavier.LA_GAMME_2, (float) 0.5),
+                    new Note(Clavier.DO_GAMME_2, 1),
+                    new Note(Clavier.SI_GAMME_2, (float) 0.5),
+                    new Note(Clavier.LA_GAMME_2, (float) 0.5),
+                    new Note(Clavier.MI_GAMME_2, 1),
+                    new Note(Clavier.MI_GAMME_2, 1),
+                    new Note(Clavier.MI_GAMME_2, 1),
+                    new Note(Clavier.RE_GAMME_2, (float) 0.5),
+                    new Note(Clavier.MI_GAMME_2, (float) 0.5),
+                    new Note(Clavier.FA_GAMME_2, (float) 0.5),
+                    new Note(Clavier.FA_GAMME_2, 2),
+                    new Note(Clavier.FA_GAMME_2, (float) 0.5),
+                    new Note(Clavier.MI_GAMME_2, (float) 0.5),
+                    new Note(Clavier.RE_GAMME_2, (float) 0.5),
+                    new Note(Clavier.FA_GAMME_2, (float) 0.5),
+                    new Note(Clavier.MI_GAMME_2, 2),
+                    new Note(Clavier.MI_GAMME_2, (float) 0.5),
+                    new Note(Clavier.RE_GAMME_2, (float) 0.5),
+                    new Note(Clavier.DO_GAMME_2, (float) 0.5),
+                    new Note(Clavier.SI_GAMME_2, 1),
+                    new Note(Clavier.MI_GAMME_2, 1),
+                    new Note(Clavier.DO_GAMME_2, 1),
+                    new Note(Clavier.SI_GAMME_2, 1),
+                    new Note(Clavier.LA_GAMME_2, 2)
+
+            };
+
+            for(int i = 0; i < notesBellaCiao.length; i++)
+            {
+                touche = findViewById(notesBellaCiao[i].getTouche().getIdImage());
+
+                activerTouche();
+
+                try
+                {
+                    Thread.sleep( (long)(notesBellaCiao[i].getDuree() * 1000) );
+                }
+                catch(InterruptedException ie)
+                {
+                    Log.e(LOG_TAG, "Échec du Thread.sleep()");
+                }
+
+                desactiverTouche(notesBellaCiao[i].getTouche().estNoire());
+            }
+
+            Log.e(LOG_TAG, "Fin de la mélodie");
+
+        }
+
+    }
+
 }
