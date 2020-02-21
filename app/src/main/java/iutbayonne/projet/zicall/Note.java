507566e6d40149d9f0c1d3c0fda5dc0a9fd77a04
Christopher <mirambeau.lcd@gmail.com>
2020-02-21 08:40:31 +0100

    Création des classes Touche, Note et Clavier pour la fonctionnalité 'Apprentissage du piano'

---
+++ app/src/main/java/iutbayonne/projet/zicall/Note.java
@@ -0,0 +1,34 @@
+package iutbayonne.projet.zicall;
+
+public class Note
+{
+
+    private float duree;
+    private Touche maTouche;
+
+    public Note(Touche uneTouche, float duree)
+    {
+        this.duree = duree;
+        this.maTouche = uneTouche;
+    }
+
+    public float getDuree()
+    {
+        return duree;
+    }
+
+    public void setDuree(float duree)
+    {
+        this.duree = duree;
+    }
+
+    public Touche getTouche()
+    {
+        return maTouche;
+    }
+
+    public void setTouche(Touche maTouche)
+    {
+        this.maTouche = maTouche;
+    }
+}
