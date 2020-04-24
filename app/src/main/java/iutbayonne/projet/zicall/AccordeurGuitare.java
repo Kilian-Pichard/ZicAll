dc50ad782df58f02fa576864549a782584d7ec16
Kilian Pichard <utilisateur@MacBook-VAAMKONDAT.local>
2020-04-24 14:01:12 +0200

    Ajout d'une case à cocher auto pour mettre l'accordage en automatique

--- app/src/main/java/iutbayonne/projet/zicall/AccordeurGuitare.java
+++ app/src/main/java/iutbayonne/projet/zicall/AccordeurGuitare.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.cardview.widget.CardView;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.text.Html;
 import android.view.Menu;
@@ -11,6 +12,7 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.CheckBox;
 import be.tarsos.dsp.AudioDispatcher;
 import be.tarsos.dsp.AudioEvent;
 import be.tarsos.dsp.AudioProcessor;
@@ -67,6 +69,11 @@ public class AccordeurGuitare extends AppCompatActivity
      */
     private AffichageFrequence affichage;
 
+    /**
+     * Case à cocher qui permet de mettre l'accordeur de guitare en automatique
+     */
+    private CheckBox auto;
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
@@ -84,6 +91,7 @@ public class AccordeurGuitare extends AppCompatActivity
         this.sol = findViewById(R.id.sol);
         this.si = findViewById(R.id.si);
         this.mi2 = findViewById(R.id.mi2);
+        this.auto = findViewById(R.id.checkBox_auto);
         this.erreurGauche = findViewById(R.id.erreurGauche);
         this.erreurDroite = findViewById(R.id.erreurDroite);
         this.frequenceMesuree = findViewById(R.id.frequenceMesure);
@@ -104,6 +112,7 @@ public class AccordeurGuitare extends AppCompatActivity
         sol.setOnClickListener(new SelectionCorde());
         si.setOnClickListener(new SelectionCorde());
         mi2.setOnClickListener(new SelectionCorde());
+        auto.setOnClickListener(new SelectionCorde());
 
         // Relie l'AudioDispatcher à l'entrée par défaut du smartphone (micro)
         dispatcher = AudioDispatcherFactory.fromDefaultMicrophone(22050,1024,0);
@@ -128,6 +137,9 @@ public class AccordeurGuitare extends AppCompatActivity
         // Création et lancement du thread pour récupérer la fréquence
         audioThread = new Thread(dispatcher, "Audio Thread");
         audioThread.start();
+
+        auto.setChecked(true);
+        frequenceReference.setText("...");
     }
 
     /**
@@ -135,41 +147,118 @@ public class AccordeurGuitare extends AppCompatActivity
      */
     private class SelectionCorde implements View.OnClickListener
     {
+        public void backgroudWhite(){
+            mi.setCardBackgroundColor(Color.parseColor("#FEFFFF"));
+            la.setCardBackgroundColor(Color.parseColor("#FEFFFF"));
+            re.setCardBackgroundColor(Color.parseColor("#FEFFFF"));
+            sol.setCardBackgroundColor(Color.parseColor("#FEFFFF"));
+            si.setCardBackgroundColor(Color.parseColor("#FEFFFF"));
+            mi2.setCardBackgroundColor(Color.parseColor("#FEFFFF"));
+        }
+
         @Override
         public void onClick(View v)
         {
             switch(v.getId())
             {
                 case R.id.mi:
+                    auto.setChecked(false);
+                    backgroudWhite();
+                    mi.setCardBackgroundColor(Color.parseColor("#4A9CCE"));
                     cordeSelectionnee = cordeMi;
+                    frequenceReference.setText(String.valueOf(cordeSelectionnee.getFrequenceReferenceCorde()));
                     break;
 
                 case R.id.la:
+                    auto.setChecked(false);
+                    backgroudWhite();
+                    la.setCardBackgroundColor(Color.parseColor("#4A9CCE"));
                     cordeSelectionnee = cordeLa;
+                    frequenceReference.setText(String.valueOf(cordeSelectionnee.getFrequenceReferenceCorde()));
                     break;
 
                 case R.id.re:
+                    auto.setChecked(false);
+                    backgroudWhite();
+                    re.setCardBackgroundColor(Color.parseColor("#4A9CCE"));
                     cordeSelectionnee = cordeRe;
+                    frequenceReference.setText(String.valueOf(cordeSelectionnee.getFrequenceReferenceCorde()));
                     break;
 
                 case R.id.sol:
+                    auto.setChecked(false);
+                    backgroudWhite();
+                    sol.setCardBackgroundColor(Color.parseColor("#4A9CCE"));
                     cordeSelectionnee = cordeSol;
+                    frequenceReference.setText(String.valueOf(cordeSelectionnee.getFrequenceReferenceCorde()));
                     break;
 
                 case R.id.si:
+                    auto.setChecked(false);
+                    backgroudWhite();
+                    si.setCardBackgroundColor(Color.parseColor("#4A9CCE"));
                     cordeSelectionnee = cordeSi;
+                    frequenceReference.setText(String.valueOf(cordeSelectionnee.getFrequenceReferenceCorde()));
                     break;
 
                 case R.id.mi2:
+                    auto.setChecked(false);
+                    backgroudWhite();
+                    mi2.setCardBackgroundColor(Color.parseColor("#4A9CCE"));
                     cordeSelectionnee = cordeMiAigu;
+                    frequenceReference.setText(String.valueOf(cordeSelectionnee.getFrequenceReferenceCorde()));
+                    break;
+
+                case R.id.checkBox_auto:
+                    backgroudWhite();
+                    if(((CheckBox)v).isChecked()) {
+                        frequenceReference.setText("...");
+                        if( (frequenceDetectee >= cordeMi.getFrequenceReferenceCorde()-10) && (frequenceDetectee >= cordeMi.getFrequenceReferenceCorde()+10) ){
+                            mi.setCardBackgroundColor(Color.parseColor("#4A9CCE"));
+                            cordeSelectionnee = cordeMi;
+                            frequenceReference.setText(String.valueOf(cordeSelectionnee.getFrequenceReferenceCorde()));
+                        }
+                        if( (frequenceDetectee >= cordeLa.getFrequenceReferenceCorde()-10) && (frequenceDetectee >= cordeLa.getFrequenceReferenceCorde()+10) ){
+                            la.setCardBackgroundColor(Color.parseColor("#4A9CCE"));
+                            cordeSelectionnee = cordeLa;
+                            frequenceReference.setText(String.valueOf(cordeSelectionnee.getFrequenceReferenceCorde()));
+                        }
+                        if( (frequenceDetectee >= cordeRe.getFrequenceReferenceCorde()-10) && (frequenceDetectee >= cordeRe.getFrequenceReferenceCorde()+10) ){
+                            re.setCardBackgroundColor(Color.parseColor("#4A9CCE"));
+                            cordeSelectionnee = cordeRe;
+                            frequenceReference.setText(String.valueOf(cordeSelectionnee.getFrequenceReferenceCorde()));
+                        }
+                        if( (frequenceDetectee >= cordeSol.getFrequenceReferenceCorde()-10) && (frequenceDetectee >= cordeSol.getFrequenceReferenceCorde()+10) ){
+                            sol.setCardBackgroundColor(Color.parseColor("#4A9CCE"));
+                            cordeSelectionnee = cordeSol;
+                            frequenceReference.setText(String.valueOf(cordeSelectionnee.getFrequenceReferenceCorde()));
+                        }
+                        if( (frequenceDetectee >= cordeSi.getFrequenceReferenceCorde()-10) && (frequenceDetectee >= cordeSi.getFrequenceReferenceCorde()+10) ){
+                            si.setCardBackgroundColor(Color.parseColor("#4A9CCE"));
+                            cordeSelectionnee = cordeSi;
+                            frequenceReference.setText(String.valueOf(cordeSelectionnee.getFrequenceReferenceCorde()));
+                        }
+                        if( (frequenceDetectee >= cordeMiAigu.getFrequenceReferenceCorde()-10) && (frequenceDetectee >= cordeMiAigu.getFrequenceReferenceCorde()+10) ){
+                            mi2.setCardBackgroundColor(Color.parseColor("#4A9CCE"));
+                            cordeSelectionnee = cordeMiAigu;
+                            frequenceReference.setText(String.valueOf(cordeSelectionnee.getFrequenceReferenceCorde()));
+                        }
+
+                    }
+                    else{
+                        mi.setCardBackgroundColor(Color.parseColor("#4A9CCE"));
+                        cordeSelectionnee = cordeMi;
+                        frequenceReference.setText(String.valueOf(cordeSelectionnee.getFrequenceReferenceCorde()));
+                    }
                     break;
             }
 
-            frequenceReference.setText(String.valueOf(cordeSelectionnee.getFrequenceReferenceCorde()));
+            //frequenceReference.setText(String.valueOf(cordeSelectionnee.getFrequenceReferenceCorde()));
             arreterAffichage();
             affichage = new AffichageFrequence();
         }
     }
+
 
     /**
      * Thread qui se charge d'afficher en continu des indications
@@ -212,6 +301,7 @@ public class AccordeurGuitare extends AppCompatActivity
         }
     }
 
+
     /**
      * Affiche à l'écran la fréquence détectée par le microphone, ainsi qu'une indication pour
      * dire à l'utilisateur de tendre ou détendre la corde de sa guitare.
@@ -223,11 +313,13 @@ public class AccordeurGuitare extends AppCompatActivity
 
         }
 
+
         /**
          * Méthode à exécuter à la création du Runnable.
          */
         public void run()
         {
+
             // Si une fréquence est mesurée
             if(frequenceDetectee != -1)
             {
